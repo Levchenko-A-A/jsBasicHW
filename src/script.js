5283bafcee477cfa7a6845105e2a4f2b564966a1
Lev-a-a <lev_a_a@mail.ru>
2025-10-28 21:48:10 +0200

    Решение третий задачи 1.6 Цикл

--- src/script.js
+++ src/script.js
@@ -10,3 +10,17 @@ for(let i=1; i<=9; i++)
     console.log(`7 * ${i} = ${7*i}`);
 }
 console.log('');
+console.log('Задача №3');
+const number = prompt('Введите число N:');
+let sum = 0;
+let count = 0;
+for(let i=1; i<=number; i++)
+{
+    if(i%2!=0){
+        sum += i;
+        count++;
+    }
+}
+console.log(sum);
+console.log(count);
+console.log(`Среднее арифметическое всех нечетных чисел: ${(sum/count).toFixed(2)}`);

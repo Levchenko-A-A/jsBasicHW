97eb673638f34d47ae5cd1c72126af1026b526de
Lev-a-a <lev_a_a@mail.ru>
2025-10-28 21:47:32 +0200

    Решение второй задачи 1.6 Цикл

--- src/script.js
+++ src/script.js
@@ -4,3 +4,9 @@ for(let i=50; i<=100; i++)
     console.log(i);
 }
 console.log('');
+console.log('Задача №2');
+for(let i=1; i<=9; i++)
+{
+    console.log(`7 * ${i} = ${7*i}`);
+}
+console.log('');
